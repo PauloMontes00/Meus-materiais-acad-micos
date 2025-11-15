832d5f1e771a0c0f7d6dcf2675023f869efa2832
PauloMontes00 <paulomontes005@gmail.com>
2025-11-14 23:04:43 -0300

    hoje ta pago :3

--- src/lista02/questao01.java
+++ src/lista02/questao01.java
@@ -1,4 +1,23 @@
 package lista02;
 
+import java.util.Scanner;
+
 public class questao01 {
+    public static void main (String [] args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Informe um numero:");
+        int numero = sc.nextInt();
+
+        numero = Math.abs(numero);
+
+        int soma= 0;
+        while (numero > 0) {
+            soma += numero% 10;
+            numero /= 10;
+        }
+        System.out.println("A soma dos digitos é: " + soma);
+        sc.close();
+    }
 }
+//Escreva um programa que calcule a soma de todos os dígitos de um número inteiro fornecido pelo usuário.
---
+++ src/lista02/questao02.java
@@ -0,0 +1,10 @@
+package lista02;
+
+import java.util.Scanner;
+
+public class questao02 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+    }
+}
+//Faça um programa que calcule a soma dos N primeiros termos da sequência, com N dado pelo usuário
